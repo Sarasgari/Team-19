3bbdfb77e122c0e61b1704aeeb5685249d8535a7
8Doc <qnlineq@gmail.com>
2024-10-24 17:11:15 +0100

    added comment to min.js

--- Maths/min.js
+++ Maths/min.js
@@ -1,12 +1,17 @@
+/********************************
+Developer: Mayaz Karim
+University ID: 230712279
+Function: This function takes two inputs as integers and returns the minimum
+********************************/
+
 function min(num1, num2) {
     return (num1 < num2) ? num1 : num2;
 }
 
-const input1 = prompt("Enter first number:");
-const input2 = prompt("Enter second number:");
-
-const num1 = parseInt(input1);
-const num2 = parseInt(input2);
+const inputNum1 = prompt("Enter first number:");
+const inputNum2 = prompt("Enter second number:");
+const num1 = parseInt(inputNum1);
+const num2 = parseInt(inputNum2);
 
 if (Number.isInteger(num1 && Number.isInteger(num2))) {
     const result = min(num1, num2);
