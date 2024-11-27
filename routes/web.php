6caed59694424c9c00724a74ce4830a91d4c1109
Mohammed Rahman <mohammedrahman046@gmail.com>
2024-11-27 11:28:34 +0000

    updated routing and controller class

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
 
+
+//URLs
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/games', function(){
+    return view('products');
+});
+
+//in page
+Route::get('/producs', [PageController::class, 'products'])->name('products');
