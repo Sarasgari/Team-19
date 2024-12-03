8c8584f0a39d42e2f8ca5c703788ad0823a91479
Abdulraman Mostafa <230046694@aston.ac.uk>
2024-12-03 12:16:07 +0000

    adding payment page

---
+++ resources/views/payment.blade.php
@@ -0,0 +1,20 @@
+<!--
+  Developer: Abdulrahman Mostafa
+  University ID: 2300466694
+  Function: Payment page showes thst the payment went successful and smoothly
+-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Payment Successful</title>
+    <link rel="stylesheet" href="Payment.css">
+</head>
+<body>
+    <div class="success-container">
+        <h1>Payment Successful</h1>
+        <div class="tick-icon">&#10004;</div>
+    </div>
+</body>
+</html>
