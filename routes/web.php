5b0271d36b03a0faf5693b7f2b1cccbea2e50919
Abdulraman Mostafa <230046694@aston.ac.uk>
2024-12-08 22:12:35 +0000

    adding routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,17 @@ Route::get('/products', [PageController::class, 'products'])->name('products');
 Route::get('/basket', [PageController::class, 'basket'])->name('Basket');
 
 Route::get('/aboutus', [PageController::class, 'aboutus'])->name('aboutus');
+
+Route::get('/contactus', [PageController::class, 'contactus'])->name('contactus');
+
+Route::get('/signup', [PageController::class, 'signup'])->name('signup');
+
+Route::get('/login', [PageController::class, 'login'])->name('login');
+
+Route::get('/home', [PageController::class, 'home'])->name('home');
+
+Route::get('/paymentform', [PageController::class, 'paymentform'])->name('paymentform');
+
+Route::get('/payment', [PageController::class, 'payment'])->name('payment');
+
+    
