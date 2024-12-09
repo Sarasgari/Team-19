838b86e6e6c3b84f6a3a46bfb60a76c36c2bb175
Mohammed Rahman <mohammedrahman046@gmail.com>
2024-12-09 12:58:40 +0000

    Added laravel/ui

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\BasketController;
+use App\Http\Controllers\AuthController;
 
 
 //URLs
@@ -9,12 +11,15 @@ Route::get('/', function () {
     return view('home');
 });
 
+Auth::routes();
 
 //in page
 Route::get('/products', [PageController::class, 'products'])->name('products');
-
 Route::get('/basket', [PageController::class, 'basket'])->name('Basket');
-
 Route::get('/aboutus', [PageController::class, 'aboutus'])->name('aboutus');
-
 Route::get('/', [PageController::class, 'home'])->name('home');
+
+//basket commands
+Route::post('cart/add/{games}',[BasketController::class, 'add'])->name('cart.add');
+Route::post('cart/remove/{games}',[BasketController::class, 'remove'])->name('cart.remove');
+Route::post('cart/update',[BasketController::class, 'update'])->name('cart.update');
