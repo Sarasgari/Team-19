1ee4812145c26f3b0b8801b00e36fa6296141cee
Mohammed Rahman <mohammedrahman046@gmail.com>
2025-02-08 14:02:49 +0000

    Improved layout

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,33 +1,31 @@
-Developer: Eyad Al Saher
+<!--Developer: Eyad Al Saher
 University ID: 230047989
 Function : Login page enable the login to the website and enjoy with the features
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Responsive Login Form</title>
-    <link rel="stylesheet" href="{{ asset('css/stylesheet.css') }}">
-</head>
-<body>
-    <style>body {background-image: url("{{ asset('image/abstract.jpg') }}")}</style>
-    <form class="form" action="" method="POST">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/SignupLogin.css') }}">
-</head>
-<body>
-    <form class="form" action="" method="post">
-        <h2>Login</h2>
-        
-        <label for="email">Email</label>
-        <input type="email" id="email" name="email" placeholder="Enter your email" required>
-        
-        <label for="password">Password</label>
-        <input type="password" id="password" name="password" placeholder="Enter your password" required>
-        <button type="submit">Login</button>
-        <p>Don't have account ? <a href="{{ route('signup') }}">Signup</a></p>
-        <a href="{{ route('home') }}" class="btn btn-primary w-100">Login</a>
-        <a href="{{ route('home') }}" class="btn btn-secondary mt-3">Back to Home</a>
-    </form>
-</body>
-</html>
+-->
+@extends('layout')
+@section('title','Log in')
+
+@section('body')
+
+    <div class="container mx-auto text-center">
+        <form class="form" action="" method="POST">
+            <h2>Log In</h2>
+
+            <div class="row mb-3">
+                <label for="email" class="col-sm-2 col-form-label">Email</label>
+                <input type="email" id="email" name="email" placeholder="Enter your email" required>
+            </div>
+            
+            <div class="row mb-3">
+                <label for="password" class="col-sm-2 col-form-label">Password</label>
+                <input type="password" id="password" name="password" placeholder="Enter your password" required>
+            </div>
+            
+            <button class="btn btn-primary" type="submit">Login</button>
+            
+            <p>Don't have an account ? <a href="{{ route('signup') }}">Signup</a></p>
+            <a href="{{ route('home') }}" class="btn btn-secondary mt-3">Back to Home</a>
+        </form>
+    </div>
+    
+@endsection
--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,12 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 
+@extends('layout')
+@section('title','Products')
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>GameDen - Your Gaming Paradise</title>
-
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
   <style>
     
     body {
@@ -114,31 +111,7 @@
   </style>
 </head>
 
-<body>
-   <!-- Navbar -->
-   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container">
-      <div>
-      <a class="navbar-brand" href="#">
-        <img src="{{ asset('image/logo-removebg.png') }}" alt="GameDen" class="d-inline-block align-text-top" style="height: 40px;">
-        GameDen
-      </a>
-    </div>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav ms-auto">
-        <li class="nav-item"><a class="nav-link active" href="{{ route('home') }}">Home</a></li>
-        <li class="nav-item"><a class="nav-link" href="{{ route('products') }}">Products</a></li>
-        <li class="nav-item"><a class="nav-link" href="{{ route('aboutus') }}">About Us</a></li>
-        <li class="nav-item"><a class="nav-link" href="#">Contact</a></li>
-        <li class="nav-item"><a class="nav-link" href="{{route('Basket')}}">Cart</a></li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-
+@section('body')
   <!-- Main title for the page  -->
   <div class="container">
     <div class="main-title">Games</div>
@@ -627,7 +600,4 @@
     </div>
   </div>
 </div>
-
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
+@endsection
