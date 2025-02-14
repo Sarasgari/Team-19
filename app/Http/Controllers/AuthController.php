3dc38daba46d5e095c38c5235507720238621fcf
Mohammed Rahman <mohammedrahman046@gmail.com>
2025-02-14 13:08:46 +0000

    Included session class

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
