41bf3bc1f0b36e542d263300b5e1b641f23dae9e
Mohammed Rahman <mohammedrahman046@gmail.com>
2025-02-14 21:57:51 +0000

    updated header

--- resources/views/include/header.blade.php
+++ resources/views/include/header.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">
+    <a class="navbar-brand" href="{{route('home')}}">
       <img src="{{ asset('image/logo-removebg.png') }}" alt="GameDen Logo" class="d-inline-block align-text-top" style="height: 40px;">
       GameDen
     </a>
@@ -22,12 +22,17 @@
         <li class="nav-item">
           <a class="nav-link" href="#">Contact Us</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="{{route('login')}}">Log In</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="{{route('logout')}}">Log Out</a>
-        </li>
+        @guest
+          <li class="nav-item"><a class="nav-link" href="{{ url('/login') }}">Login</a></li>
+          <li class="nav-item"><a class="nav-link btn btn-primary text-white" href="{{ url('/signup') }}">Sign Up</a></li>
+        @else
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              Welcome, {{ auth()->user()->name }}!
+            </a>
+          </li>
+          <li class="nav-item"><a class="nav-link btn btn-danger text-white" href="{{ url('/logout') }}">Logout</a></li>
+        @endguest
         <!--shopping bag-->
         <li class="nav-item"><a class="nav-link" href="{{ route('Basket') }}">
           <i class="fas fa-shopping-bag"></i>
