e9d097b00f143d3d7fb2c80f1a14c02598177ff8
Haider1jr <230069903@aston.ac.uk>
2025-03-04 15:39:35 +0000

    chat bot
    haider

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,8 +22,18 @@
 <body>
   <!-- Navbar -->
   @include('include.header')
+  <div id="google_translate_element"></div>
+
+<script type="text/javascript">
+function googleTranslateElementInit() {
+}
+</script>
+
   <!-- Hero Section -->
   <section class="hero-section position-relative">
+  
+</script>
+
     <!-- Background Video -->
     <video class="hero-video" autoplay loop muted playsinline>
       <source src="{{ asset('image/herovideo.mp4') }}" type="video/mp4">
@@ -229,9 +239,13 @@
     </div>
   </footer>
 
-  <!-- Bootstrap JS -->
+  <!-- chatot -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
   <script src="script.js"></script>
+  <script>
+(function(){if(!window.chatbase||window.chatbase("getState")!=="initialized"){window.chatbase=(...arguments)=>{if(!window.chatbase.q){window.chatbase.q=[]}window.chatbase.q.push(arguments)};window.chatbase=new Proxy(window.chatbase,{get(target,prop){if(prop==="q"){return target.q}return(...args)=>target(prop,...args)}})}const onLoad=function(){const script=document.createElement("script");script.src="https://www.chatbase.co/embed.min.js";script.id="9PWNMB1p2CzJaFsLkUCRE";script.domain="www.chatbase.co";document.body.appendChild(script)};if(document.readyState==="complete"){onLoad()}else{window.addEventListener("load",onLoad)}})();
+</script>
+
   
 </body>
 </html>
