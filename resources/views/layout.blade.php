49ebd1df8afc5a8efe50f5c361d1328d3c5c5f2d
Sarasgari <sarasgari82@gmail.com>
2025-03-06 15:11:38 +0000

    navbar fixed

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,11 +4,15 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title','GameDen')</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
+  <link href="{{ asset('css/style.css') }}" rel="stylesheet">
   </head>
   <body>
     @include('include.header')
     @yield('body')
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" ></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" ></script>
   </body>
 </html>
