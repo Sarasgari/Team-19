d85b819ab201211cebd43cb84d85f1507a9e27cb
Sarasgari <sarasgari82@gmail.com>
2025-03-19 14:22:23 +0000

    nothing

--- resources/views/Basket.blade.php
+++ resources/views/Basket.blade.php
@@ -14,27 +14,8 @@
   <link rel="stylesheet" href="{{ asset('css/Basket.css') }}">
 </head>
 <body>
-  <!-- Navbar -->
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container">
-      <a class="navbar-brand" href="#">
-        <img src="{{ asset('image/logo.png') }}" alt="GameDen Logo" class="d-inline-block align-text-top" style="height: 40px;">
-        GameDen
-      </a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav ms-auto">
-          <li class="nav-item"><a class="nav-link active" href="{{route('home')}}">Home</a></li>
-          <li class="nav-item"><a class="nav-link" href="{{ route('products') }}">Products</a></li>
-          <li class="nav-item"><a class="nav-link" href="{{ route('aboutus') }}">About Us</a></li>
-          <li class="nav-item"><a class="nav-link" href="{{ route('contactus') }}">Contact</a></li>
-          <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">Register</a></li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+  
+@include('include.header')
 
   <!-- Main Content -->
   <main>
