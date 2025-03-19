2b8fd77c05f83f154c70561663ca489a0c126c92
Minwoo Noh <shalsdn456@naver.com>
2025-03-19 22:04:27 +0000

    fix lougour error in admin page

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -35,7 +35,14 @@ class LoginController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('guest')->except('logout');
-        $this->middleware('auth')->only('logout');
+    $this->middleware('guest')->except(['logout']);
     }
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('/');
+    }
+    
 }
--- resources/views/admin/profile.blade.php
+++ resources/views/admin/profile.blade.php
@@ -181,7 +181,14 @@
             <li class="nav-item"><a class="nav-link text-white" href="{{route('admin.settings')}}"><i class="fas fa-cog me-2"></i> Settings</a></li>
         </ul>
         <div class="mt-auto text-center">
-            <a class="nav-link text-danger" href="#"><i class="fas fa-sign-out-alt me-2"></i> Logout</a>
+            <a class="nav-link text-danger" href="{{ route('logout') }}"
+                onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                <i class="fas fa-sign-out-alt me-2"></i> Logout
+            </a>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+            </form>
+
         </div>
     </nav>
 
